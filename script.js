c1bd85ee0b4791b32124317b93d5c0e235a809f2
Mohit Parmar <127684291+mrmohitparmar@users.noreply.github.com>
2025-12-10 11:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here
+// Get references
+let btn = document.getElementById("addTodoBtn");
+let input = document.getElementById("newTodoInput");
+let ol = document.getElementById("todoList");
 
+// Add todo function
+function add() {
+    let v = input.value.trim();
+    if (v !== "") {
+        let li = document.createElement("li"); // create li element
+        li.textContent = v;                    // set text
+        ol.appendChild(li);                    // append to ol
+        input.value = "";                       // clear input
+    }
+}
+
+// Add event listener
+btn.addEventListener("click", add);
